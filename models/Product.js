ae6eacd3cb38bc357e853c3acd0bfc6165e66451
HoangTung17 <tung170100@gmail.com>
2022-04-29 09:30:04 +0700

    post

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+//var mongoDB = "mongodb://localhost:27017/NoSQLBoosterSamples"
+var mongoDB = "mongodb+srv://tung17:tung17012000@cluster0.evpzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+
+const ProductSchema = new Schema({
+    name : {type:String, required:true},
+    price: {type:Number},
+    picURL: {type:String}
+
+})
+
+module.exports = mongoose.model('Product', ProductSchema);
