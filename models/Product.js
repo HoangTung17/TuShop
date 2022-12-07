47a6c53dbbc48dd0bc7740172230ccd821b95122
HoangTung17 <tung170100@gmail.com>
2022-12-07 15:15:03 +0700

    2022

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 
 //var mongoDB = "mongodb://localhost:27017/NoSQLBoosterSamples"
 var mongoDB = "mongodb+srv://tung17:tung17012000@cluster0.evpzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//var mongoDB = "mongodb+srv://tung17:tung170120002@cluster0.q7wzbzy.mongodb.net/test"
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
